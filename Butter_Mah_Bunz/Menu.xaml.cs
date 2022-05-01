3242d5dd03d099f7414d1b7f9ba50107ba919ace
jp8117 <jp8117@live.com>
2022-05-01 15:17:34 -0400

    Integrated new custom buttons

--- Butter_Mah_Bunz/Menu.xaml.cs
+++ Butter_Mah_Bunz/Menu.xaml.cs
@@ -36,9 +36,14 @@ namespace Butter_Mah_Bunz
                     else
                         fItButton.Background = new SolidColorBrush(CoreComponents.Beef);
 
+                    //create a quickAddButton
+                    AddToCartButton quickAdd = new AddToCartButton(menuItems[index], this);
+                    quickAdd.Content = "Quick Add";
+
                     //add newly created button to the menuBox stack panel
                     MenuBox.Height += fItButton.ActualHeight;
                     MenuBox.Children.Add(fItButton);
+                    MenuBox.Children.Add(quickAdd);
                 }
             }
         }
@@ -65,61 +70,5 @@ namespace Butter_Mah_Bunz
             else
                 System.Windows.MessageBox.Show("Thy buns remain barren (Cart is empty).", "Cart Empty");
         }
-    }
-    class ItemDetailsButton : Button
-    {
-        private Backend.Item _item; 
-        public Backend.Item Item
-        {
-            get { return _item; }
-        }
-        public ItemDetailsButton(Backend.Item item)
-        {
-            _item = item;
-
-            //create and format item Label
-            System.Windows.Controls.Label label = new System.Windows.Controls.Label();
-            label.Content = item.Name;
-            label.Width = 414;
-            label.FontSize = 30;
-            label.HorizontalContentAlignment = System.Windows.HorizontalAlignment.Center;
-
-            //create and format item image
-            System.Windows.Controls.Image img = new System.Windows.Controls.Image();
-            img.Width = 150;
-            try //tries to load the image file
-            {
-                string url = "pack://application:,,,/Butter_Mah_Bunz;component/" + item.ImageURL;
-                Uri uri = new Uri(url);
-                img.Source = new BitmapImage(uri);
-            }
-            catch //if the file is not found this will catch the exception and load a defualt image
-            {
-                img.Source = new BitmapImage(new Uri("pack://application:,,,/Butter_Mah_Bunz;component/Media/bmb.png"));
-            }
-
-            //create and format description TextBlock
-            System.Windows.Controls.TextBlock description = new System.Windows.Controls.TextBlock();
-            description.Text = item.Description;
-
-            //create and format price TextBlock
-            System.Windows.Controls.TextBlock price = new System.Windows.Controls.TextBlock();
-            price.Text = item.Price.ToString("C");
-
-            //create and format stack pannel for item button components
-            System.Windows.Controls.StackPanel superStacker9000 = new System.Windows.Controls.StackPanel();
-            superStacker9000.Width = 414;
-            superStacker9000.Children.Add(label);
-            superStacker9000.Children.Add(img);
-            superStacker9000.Children.Add(description);
-            superStacker9000.Children.Add(price);
-
-            this.Content = superStacker9000;
-            this.Margin = new System.Windows.Thickness(0, 0, 0, 15);
-        }
-    }
-    public class AddToCartButton : Button
-    {
-
     }
 }
