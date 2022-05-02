fda7d91e0f4d8ed8e6c76572354f13e4d7c13490
pstephen1 <pstephen1@live.com>
2022-05-01 20:04:49 -0400

    Added functionality to pull data from cart array, format data, and display.

--- Butter_Mah_Bunz/Cart.xaml.cs
+++ Butter_Mah_Bunz/Cart.xaml.cs
@@ -23,6 +23,57 @@ namespace Butter_Mah_Bunz
         public Cart()
         {
             InitializeComponent();
+
+            if(!CoreComponents.CartEmpty)
+            {
+                string[][] cartItems = CoreComponents.CartDetails;
+
+                for (int i = 0; i < cartItems.Length; i++)
+                { 
+                    Button button = new Button();
+                    StackPanel itemStack = new StackPanel();
+                    itemStack.Width = 414;
+                    
+                    //Name left aligned, title larger. [i][1] name, 2 desc, 3 img url, 4 total string
+                    Label itemName = new Label();
+                    itemName.Content = cartItems[i][1];
+                    //Formatting
+                    itemName.Width = 300;
+                    itemName.HorizontalAlignment = HorizontalAlignment.Left;
+                    itemName.FontSize = 18;
+                    //Adds formatted content to panel
+                    itemStack.Children.Add(itemName);
+
+                    for (int j = 5; j < cartItems[i].Length; j++)
+                    {
+                        //Smaller, right aligned - Enhancements loop
+                        Label enhName = new Label();
+                        enhName.Content = cartItems[i][j];
+                        //Formatting
+                        enhName.Width = 150;
+                        enhName.HorizontalAlignment = HorizontalAlignment.Left;
+                        enhName.Margin = new Thickness(35, 5, 0, 3);
+                        enhName.FontSize = 8;
+                        //Adds formatted content to panel
+                        itemStack.Children.Add(enhName);
+                    } 
+                    //Price right-aligned, calculate periods between name and price. Larger than enhancements, smaller than name. [i][4]
+                    Label price = new Label();
+                    price.Content = cartItems[i][4];
+                    //Formatting
+                    price.Width = 75;
+                    price.HorizontalAlignment = HorizontalAlignment.Right;
+                    price.FontSize = 14;
+                    //Adds formatted content to panel
+                    itemStack.Children.Add(price);
+
+                    //Add content to button and add button to screen. Yup.
+                    button.Content = itemStack;
+                    button.VerticalAlignment = VerticalAlignment.Top;
+                    OrderDetails.Height += button.ActualHeight;
+                    OrderDetails.Children.Add(button);
+                }
+            }
         }
     }
 }
